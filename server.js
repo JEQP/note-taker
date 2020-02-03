21aa3d2f2516c703f787877322e6d68478123fd8
James Pearce <jamesquintanapearce@gmail.com>
2020-02-03 11:58:06 +1100

    post notes added

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// heroku  https://git.heroku.com/boiling-everglades-47466.git
+// https://boiling-everglades-47466.herokuapp.com/
+
 // Dependencies
 // =============================================================
 var express = require("express");
 var path = require("path");
+var fs = require("fs");
+var db = require("/db/db.json");
 
 // Sets up the Express App
 // =============================================================
@@ -24,6 +29,20 @@ app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 
+app.get("/api/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/db/db.json"));
+});
+
+app.post("/api/notes", function (req, res){
+    var newNote = req.body;
+    newNote.routename = newNote.title.replace(/\s+/g, "").toLowerCase();
+    console.log(newNote.title);
+    console.log(newNote.text);
+    db.push(newNote);
+    res.json(newNote);
+
+    
+})
 
 // Starts the server to begin listening
 // =============================================================
