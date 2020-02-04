66ff4223281640d7c1ddd76c1b192b23a835308d
James Pearce <jamesquintanapearce@gmail.com>
2020-02-04 17:30:02 +1100

    notes saved into Database

--- server.js
+++ server.js
@@ -6,17 +6,20 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var db = require("/db/db.json");
+var db = require("./db/db.json");
 
 // Sets up the Express App
 // =============================================================
 
 var app = express();
+app.use(express.static(path.join(__dirname, '/public')));
 const PORT = process.env.PORT || 5001;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/db')));
 
 // Routes
 // =============================================================
@@ -33,23 +36,41 @@ app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-app.post("/api/notes", function (req, res){
+
+
+
+// Get note json from webpage
+app.post("/api/notes", function (req, res) {
     var newNote = req.body;
     newNote.routename = newNote.title.replace(/\s+/g, "").toLowerCase();
-    console.log(newNote.title);
-    console.log(newNote.text);
-    db.push(newNote);
-    res.json(newNote);
+
+    // Get db file
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            throw err;
+        }
+        DBJSON = JSON.parse(data); // should be an array of JSON objects
+        console.log("DBJSON: " + JSON.stringify(DBJSON));
+        // append newNote to file
+
+        DBJSON.push(newNote);
+        DBStr = JSON.stringify(DBJSON); // restringifies the database so it can be stored
+        console.log(DBStr);
+
+        fs.writeFile("./db/db.json", DBStr, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+    });
+
+});
 
 
-})
-
-app.use(express.static(path.join(__dirname, '/public')));
-app.use(express.static(path.join(__dirname, '/db')));
 
 // Starts the server to begin listening
 // =============================================================
 
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-  });
+});
