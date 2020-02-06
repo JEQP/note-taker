61622c93084b562a3f8ef43a7fb332fedcd52ff7
James Pearce <jamesquintanapearce@gmail.com>
2020-02-06 17:47:36 +1100

    added delete function

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 var express = require("express");
 var path = require("path");
 var fs = require("fs");
-var db = require("./db/db.json");
+
 
 // Sets up the Express App
 // =============================================================
@@ -28,59 +28,77 @@ app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 });
 
 
-app.get("/api/notes", function (req, res) {
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+var manipulateNotes = function () {
+let DBJSON = [];
+    fs.readFile('db/db.json', 'utf8', (err, data) => {
         if (err) {
             throw err;
         }
-        DBJSON = JSON.parse(data); // should be an array of JSON objects
+        console.log("data: " + data);
+        
+        DBJSON = JSON.parse(data); 
+        return DBJSON// should be an array of JSON objects
+    });
+
+
+    app.get("/api/notes", function (req, res) {
+        console.log(DBJSON)
+        console.log("sending: " + DBJSON);
+        return res.json(DBJSON);
 
     });
-    console.log("sending: " + DBJSON);
-    // return res.json(DBJSON);
-    res.sendFile(DBJSON);
+
+    app.post("/api/notes", function (req, res) {
+        let newNote = req.body;
+        // Give each entry a unique ID, that will continue to be unique after entries are deleted.
+        newNote.id = DBJSON.length+1;
+        DBJSON.forEach(e => {
+            if (newNote.id <= e.id) {
+                newNote.id = e.id +1;
+            }
+        });
+ 
+        DBJSON.push(newNote);
+        DBStr = JSON.stringify(DBJSON); // restringifies the database so it can be stored
+        console.log(DBStr);
+
+        fs.writeFile("./db/db.json", DBStr, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+        return res.json(newNote);
+
+    });
+
+    app.delete("/api/notes/:id", function (req, res) {
+        let id = req.params.id;
+        DBJSON = DBJSON.splice(id);
+        DBStr = JSON.stringify(DBJSON);
+
+        fs.writeFile("./db/db.json", DBStr, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+        });
+        return res.json(DBJSON);
+    
+    })
+
+}
+
+
+
+// Starts the server to begin listening
+// =============================================================
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
 });
 
-
-
-
-    // Get note json from webpage
-    app.post("/api/notes", function (req, res) {
-        var newNote = req.body;
-        newNote.routename = newNote.title.replace(/\s+/g, "").toLowerCase();
-
-        // Get db file
-        fs.readFile('./db/db.json', 'utf8', (err, data) => {
-            if (err) {
-                throw err;
-            }
-            DBJSON = JSON.parse(data); // should be an array of JSON objects
-            console.log("DBJSON: " + JSON.stringify(DBJSON));
-            // append newNote to file
-
-            DBJSON.push(newNote);
-            DBStr = JSON.stringify(DBJSON); // restringifies the database so it can be stored
-            console.log(DBStr);
-
-            fs.writeFile("./db/db.json", DBStr, function (err) {
-                if (err) {
-                    return console.log(err);
-                }
-            });
-        });
-
-    });
-
-
-
-    // Starts the server to begin listening
-    // =============================================================
-
-    app.listen(PORT, function () {
-        console.log("App listening on PORT " + PORT);
-    });
+manipulateNotes();
